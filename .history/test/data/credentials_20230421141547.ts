fdfb098c397473776b464d6780e424259436fa99
OThomps <othompson@qualityworkscg.com>
2023-05-10 09:41:15 -0500

    Remove history folder also

--- .history/test/data/credentials_20230421141547.ts
+++
@@ -1,8 +0,0 @@
-module.exports =
-    //storing user login data
-    [
-        {
-            username: "test-content-administrator",
-            password: "meda2022"
-        }
-    ];
--- .history/test/data/quote.data_20230501140320.ts
+++
@@ -1,8 +0,0 @@
-//Storing data needed to complete quote component form
-
-export const quoteBlockData = {
-    title: 'Quote Block Title',
-    quote: 'A Quote written by a QA',
-    author: 'Automation Script',
-    authorTitle: 'QA Automation'
-};
