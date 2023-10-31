abcb64b1f842e5e6ab598d93a6152192e1989b17
Duran Thomas <22084776+duran-thomas@users.noreply.github.com>
2023-10-31 14:15:09 -0500

    update to carousel analytics

--- test/data/carousel.data.ts
+++ test/data/carousel.data.ts
@@ -4,11 +4,15 @@ export const carouselBlockData = {
     title: 'Carousel Title',
     headline: 'Carousel Headline',
     eyebrow: 'Carousel Eyebrow',
+    intro: 'Carousel Intro',
     list: 'Carousel List',
     content: 'Carousel Content',
     btnText: 'Carousel Button',
     url: 'https://google.com',
-    altText: 'Carousel Alt Text'
-    
+    altText: 'Carousel Alt Text',
+    link: 'Carousel Link Title',
+    quote: 'Carousel Quote',
+    quoteAuthor: 'Carousel Quote Author',
+    quoteAuthorTitle: 'CEO'
 };
 
