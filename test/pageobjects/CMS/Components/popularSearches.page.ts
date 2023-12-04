a0d9dd0ff176ade6b4f780d0916caa447f9d638e
Duran Thomas <22084776+duran-thomas@users.noreply.github.com>
2023-12-04 17:29:12 -0500

    add new tests for popular search

--- test/pageobjects/CMS/Components/popularSearches.page.ts
+++ test/pageobjects/CMS/Components/popularSearches.page.ts
@@ -23,16 +23,32 @@ class popularSearchesPage extends Page {
     return $('.messages__content')
   }
 
-  public get btnLinkDropDown(){
+  public get btnLinkDropDown1(){
     return $$('li.dropbutton-toggle button')[0]
   }
 
-  public get btnDeleteLink(){
+  public get btnDeleteLink1(){
     return $$('li.delete.dropbutton__item a')[0]
+  }
+
+  public get btnEditLink1(){
+    return $$('li.edit a')[0]
   }
 
   public get btnConfirmDelete(){
     return $('div.ui-dialog-buttonset button.button--primary')
+  }
+
+  public get btnSearch(){
+    return $('div button.mf-top-bar__button--search')
+  }
+
+  public get checkboxEnabled1(){
+    return $$('.checkbox.menu-enabled div input')[0]
+  }
+
+  public popularSearchResult(title: string){
+    return $(`a[data-analytics-click-text="${title}"]`)
   }
 
   public async addNewMenuLink(title: string, link: string){
@@ -40,6 +56,21 @@ class popularSearchesPage extends Page {
 
     await (await this.btnAddLink).click();
     await (await this.inputMenuLinkTitle).setValue(title);
+    await (await this.inputLink).setValue(link);
+    await (await this.btnSave).scrollIntoView()
+    await (await this.btnSave).click();
+  }
+
+  public async addMultiMenuLink(title: string, link: string){
+    await browser.pause(2000)
+
+    await (await this.btnAddLink).click();
+    await (await this.inputMenuLinkTitle).setValue(title);
+    await (await this.inputLink).setValue(link);
+    await (await this.btnSave).scrollIntoView()
+    await (await this.btnSave).click();
+    await (await this.btnAddLink).click();
+    await (await this.inputMenuLinkTitle).setValue(`2nd ${title}`);
     await (await this.inputLink).setValue(link);
     await (await this.btnSave).scrollIntoView()
     await (await this.btnSave).click();
--- test/specs/components/popularSearches.e2e.ts
+++ test/specs/components/popularSearches.e2e.ts
@@ -2,7 +2,7 @@ import adminContentPage from "../../pageobjects/CMS/Login/adminContent.page";
 import { popularSearchData } from "../../data/popularSearch.data";
 import { getEnvironmentConfig } from "../../../envSelector";
 import popularSearchesPage from "../../pageobjects/CMS/Components/popularSearches.page";
-import { remote } from 'webdriverio'
+const assert = require('assert');
 
 
 describe('Popular Searches Tests', () => {
@@ -40,8 +40,8 @@ describe('Popular Searches Tests', () => {
   after(async function () {
     await adminContentPage.openPopularSearches();
     await browser.pause(1000)
-    await (await popularSearchesPage.btnLinkDropDown).click()
-    await (await popularSearchesPage.btnDeleteLink).click()
+    await (await popularSearchesPage.btnLinkDropDown1).click()
+    await (await popularSearchesPage.btnDeleteLink1).click()
     await (await popularSearchesPage.btnConfirmDelete).click();
     await expect(popularSearchesPage.successMsg).toBeDisplayed();
   });
@@ -54,7 +54,7 @@ describe('Popular Searches Tests', () => {
       await (await popularSearchesPage.btnSave).click();
       await expect(popularSearchesPage.successMsg).toHaveTextContaining('Menu Popular Searches (All) has been updated.')
 
-    })
+  })
 
   it('[S3C994] Verify that popular searches are displayed when the search box is clicked', async () => {
     await popularSearchesPage.addNewMenuLink(popularSearchData.title, popularSearchData.link);
@@ -68,8 +68,8 @@ describe('Popular Searches Tests', () => {
     const baseURL = environment.baseUrl;
     await browser.url(await baseURL);
 
-    (await $('div button.mf-top-bar__button--search')).click()
-    await expect($$('div.mf-navigation__drawer div div div div div ul li a span span')[0]).toHaveTextContaining(popularSearchData.title)
+    await (await popularSearchesPage.btnSearch).click()
+    await expect(await popularSearchesPage.popularSearchResult(popularSearchData.title)).toHaveTextContaining(popularSearchData.title)
   })
 
   it('[S3C995] Verify that the search results are populated when a popular search term is clicked', async () => {
@@ -85,11 +85,86 @@ describe('Popular Searches Tests', () => {
     await browser.url(await baseURL);
 
     await browser.pause(2000);
-    (await $('div button.mf-top-bar__button--search')).click();
-    await $('a[data-analytics-click-text="Pulmonary"]').waitForExist();
-    await $('a[data-analytics-click-text="Pulmonary"]').click();
+    await (await popularSearchesPage.btnSearch).click()
+    await (await popularSearchesPage.popularSearchResult(popularSearchData.title)).waitForExist();
+    await (await popularSearchesPage.popularSearchResult(popularSearchData.title)).click();
     const searchResultTitle = await ($$('h4.mf-result-item__headline')[0]).getText()
     await expect(searchResultTitle).toContain(popularSearchData.title)
   })
+
+  it('[S3C998] Verify admin can edit popular search item', async () => {
+    await popularSearchesPage.addNewMenuLink(popularSearchData.title, popularSearchData.link);
+    await expect(popularSearchesPage.successMsg).toHaveTextContaining('The menu link has been saved.');
+    await (await popularSearchesPage.btnSave).scrollIntoView();
+    await (await popularSearchesPage.btnSave).click();
+    await expect(popularSearchesPage.successMsg).toHaveTextContaining('Menu Popular Searches (All) has been updated.')
+    await browser.pause(2000);
+
+    const environment = getEnvironmentConfig(process.env.ENV);
+    const baseURL = environment.baseUrl;
+    await browser.url(await baseURL);
+
+    await (await popularSearchesPage.btnSearch).click()
+    await expect(await popularSearchesPage.popularSearchResult(popularSearchData.title)).toHaveTextContaining(popularSearchData.title)
+    await adminContentPage.openPopularSearches();
+    await (await popularSearchesPage.btnEditLink1).click();
+    await (await popularSearchesPage.inputMenuLinkTitle).setValue('Updated Pulmonary Link')
+    await (await popularSearchesPage.btnSave).scrollIntoView()
+    await (await popularSearchesPage.btnSave).click();
+    await (await popularSearchesPage.btnSave).scrollIntoView();
+    await (await popularSearchesPage.btnSave).click();
+    await expect(popularSearchesPage.successMsg).toHaveTextContaining('Menu Popular Searches (All) has been updated.')
+    await browser.pause(2000);
+
+    await browser.url(await baseURL);
+
+    await (await popularSearchesPage.btnSearch).click()
+    await expect(await popularSearchesPage.popularSearchResult('Updated Pulmonary Link')).toHaveTextContaining('Updated Pulmonary Link')
+  })
+
+  it('[S3C1008] Verify enabling and disabling of Popular-Search item', async () => {
+    await popularSearchesPage.addNewMenuLink(popularSearchData.title, popularSearchData.link);
+    await expect(popularSearchesPage.successMsg).toHaveTextContaining('The menu link has been saved.');
+    await (await popularSearchesPage.btnSave).scrollIntoView();
+    await (await popularSearchesPage.btnSave).click();
+    await expect(popularSearchesPage.successMsg).toHaveTextContaining('Menu Popular Searches (All) has been updated.')
+    await browser.pause(2000);
+
+    const environment = getEnvironmentConfig(process.env.ENV);
+    const baseURL = environment.baseUrl;
+    await browser.url(await baseURL);
+
+    await (await popularSearchesPage.btnSearch).click()
+    await expect(await popularSearchesPage.popularSearchResult(popularSearchData.title)).toHaveTextContaining(popularSearchData.title)
+    await adminContentPage.openPopularSearches();
+    await (await popularSearchesPage.checkboxEnabled1).click()
+    await (await popularSearchesPage.btnSave).scrollIntoView();
+    await (await popularSearchesPage.btnSave).click();
+    await expect(popularSearchesPage.successMsg).toHaveTextContaining('Menu Popular Searches (All) has been updated.')
+    await browser.url(await baseURL);
+    await (await popularSearchesPage.btnSearch).click()
+    const element = await $('a[data-analytics-click-text="Pulmonary"]');
+
+    try {
+      const exists = await element.isExisting();
+      assert.ok(!exists, 'Element exists on the page. Test case should failed.');
+      console.log('Element does not exist on the page. Test case should pass');
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  });
+
+  it('[S3C997] Verify admin can delete popular search item', async () => {
+    await popularSearchesPage.addMultiMenuLink(popularSearchData.title, popularSearchData.link);
+    await expect(popularSearchesPage.successMsg).toHaveTextContaining('The menu link has been saved.');
+    await (await popularSearchesPage.btnSave).scrollIntoView();
+    await (await popularSearchesPage.btnSave).click();
+    await expect(popularSearchesPage.successMsg).toHaveTextContaining('Menu Popular Searches (All) has been updated.')
+    await (await popularSearchesPage.btnLinkDropDown1).click()
+    await (await popularSearchesPage.btnDeleteLink1).click()
+    await (await popularSearchesPage.btnConfirmDelete).click();
+    await expect(popularSearchesPage.successMsg).toHaveTextContaining(`The menu link 2nd ${popularSearchData.title} has been deleted.`)
+})
 
 })
