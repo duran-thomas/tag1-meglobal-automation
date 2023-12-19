43ae7c18a973d6bd7cb81fa8830722728ba7f29a
dscotland <dscotland@qualityworkscg.com>
2023-12-19 09:18:17 -0500

    update cookie data

--- env.qaAuto.ts
+++ env.qaAuto.ts
@@ -3,13 +3,13 @@ export const environment = {
     baseUrl: 'https://meglobalode7.prod.acquia-sites.com/',
     cookies: {
         name: 'SSESSdf0d9aa5f85649894e921d4b01e00b05',
-        value: '%2CB0PBnRsnrXGgpWrDHYnmHMefJ4kSYV8dnF%2Cy3tuofPxAj2y',
+        value: 'e-dMELUKQkSItzf4eglDMMx74gBdEF2zP15H2RZWvdfZYHl7',
         domain: '.meglobalode7.prod.acquia-sites.com',
         path: '/',
     },
     admin: {
         name: 'SSESSdf0d9aa5f85649894e921d4b01e00b05',
-        value: '%2CB0PBnRsnrXGgpWrDHYnmHMefJ4kSYV8dnF%2Cy3tuofPxAj2y',
+        value: 'e-dMELUKQkSItzf4eglDMMx74gBdEF2zP15H2RZWvdfZYHl7',
         domain: '.meglobalode7.prod.acquia-sites.com',
         path: '/',
     }
