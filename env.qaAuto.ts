8395d0bd1d5d8930d786e8889c8cd4531dc7be1f
OThomps <othompson@qualityworkscg.com>
2024-01-15 12:19:21 -0500

    Latest changes

--- env.qaAuto.ts
+++ env.qaAuto.ts
@@ -3,13 +3,13 @@ export const environment = {
     baseUrl: 'https://meglobalode7.prod.acquia-sites.com/',
     cookies: {
         name: 'SSESSdf0d9aa5f85649894e921d4b01e00b05',
-        value: 'e-dMELUKQkSItzf4eglDMMx74gBdEF2zP15H2RZWvdfZYHl7',
+        value: 'zHQEH8C5ab7MFY9t7Aw0SIwRivlD9C7w5ERnAFCP5a0oQHkO',
         domain: '.meglobalode7.prod.acquia-sites.com',
         path: '/',
     },
     admin: {
         name: 'SSESSdf0d9aa5f85649894e921d4b01e00b05',
-        value: 'e-dMELUKQkSItzf4eglDMMx74gBdEF2zP15H2RZWvdfZYHl7',
+        value: 'yiHzV3sqwy-ukSM%2Cd0SFsrES%2CWDYLs5XyMfcmNU65GCg8gpF',
         domain: '.meglobalode7.prod.acquia-sites.com',
         path: '/',
     }
