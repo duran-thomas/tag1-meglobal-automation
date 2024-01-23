6f34a82d7639609ff89a41b5b5c338d7f6cc3e56
Duran Thomas <22084776+duran-thomas@users.noreply.github.com>
2024-01-23 16:35:49 -0500

    add tests for the alert bar feature

--- env.qaAuto.ts
+++ env.qaAuto.ts
@@ -3,13 +3,13 @@ export const environment = {
     baseUrl: 'https://meglobalode7.prod.acquia-sites.com/',
     cookies: {
         name: 'SSESSdf0d9aa5f85649894e921d4b01e00b05',
-        value: 'e-dMELUKQkSItzf4eglDMMx74gBdEF2zP15H2RZWvdfZYHl7',
+        value: 'sIx9drResXDxvaEzyLOFLPQbaWEWkXR1gbmC1k3ASNyaBiaM',
         domain: '.meglobalode7.prod.acquia-sites.com',
         path: '/',
     },
     admin: {
         name: 'SSESSdf0d9aa5f85649894e921d4b01e00b05',
-        value: 'e-dMELUKQkSItzf4eglDMMx74gBdEF2zP15H2RZWvdfZYHl7',
+        value: 'p7RtUufpe27z2V012Kqkr17ZdkfnhPh1cVB9pmjenMI9mWDW',
         domain: '.meglobalode7.prod.acquia-sites.com',
         path: '/',
     }
