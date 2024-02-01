6452867e59eac27bb5fb5258f0b4a98de9172012
dscotland <dscotland@qualityworkscg.com>
2024-02-01 13:09:14 -0500

    fix: updates to uat cookies

--- env.uat.ts
+++ env.uat.ts
@@ -3,15 +3,15 @@ export const environment = {
     baseUrl: 'https://uat-content.montefioreeinstein.org/',
     //unable to login
     cookies: {
-        name: 'SSESS933b071c7c2e2830c46695ffbb11e015',
-        value: 'aPo7UAcaVmDt8fj488WEY3SOlbILFmiEEPskGinXfW8skvpj',
-        domain: '.meglobaldev.prod.acquia-sites.com',
+        name: 'SSESSb3942a91b5710bb8581186c1f2753872',
+        value: 'fc6MqrtsoDtXfMrczSx0qAkDyYJVoSmQhCnCpXjXwJ88Phjw',
+        domain: '.uat-content.montefioreeinstein.org',
         path: '/',
     },
     admin: {
-        name: 'SSESSdf0d9aa5f85649894e921d4b01e00b05',
-        value: 'S7BPDRrXTWbbwil0%2Cw7JRhPrG00mCbGKRrjOyQy57CXSQEXT',
-        domain: '.meglobaldev.prod.acquia-sites.com',
+        name: 'SSESSb3942a91b5710bb8581186c1f2753872',
+        value: 'fc6MqrtsoDtXfMrczSx0qAkDyYJVoSmQhCnCpXjXwJ88Phjw',
+        domain: '.uat-content.montefioreeinstein.org',
         path: '/',
     }
 };
