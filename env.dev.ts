d9e8b39d993f35809d5f235e1858a8d928fd50fc
dscotland <dscotland@qualityworkscg.com>
2024-02-08 10:19:10 -0500

    fix: updates to cookies for dev

--- env.dev.ts
+++ env.dev.ts
@@ -3,13 +3,13 @@ export const environment = {
     baseUrl: 'https://meglobaldev.prod.acquia-sites.com/',
     cookies: {
         name: 'SSESS933b071c7c2e2830c46695ffbb11e015',
-        value: '9NkTooOKCrGxViSvQ9kCtAJLcD-GOvl5pBAzXzaaJMr9nd15',
+        value: 'w3rn1f4hN47O-5tTXlCDN3zSAYibuEg5W8VMrepO1Lmqp9WI',
         domain: '.meglobaldev.prod.acquia-sites.com',
         path: '/',
     },
     admin: {
         name: 'SSESSdf0d9aa5f85649894e921d4b01e00b05',
-        value: '9NkTooOKCrGxViSvQ9kCtAJLcD-GOvl5pBAzXzaaJMr9nd15',
+        value: 'laJggXlSEuGsl6I1V9M-nuDQNYxSFTP1XmqBZk5sdsuGPZvS',
         domain: '.meglobaldev.prod.acquia-sites.com',
         path: '/',
     }
