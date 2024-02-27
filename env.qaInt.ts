81d278926b5e4cc7733f0f0029f93380d34b9c4e
OThomps <othompson@qualityworkscg.com>
2024-02-27 13:17:30 -0500

    Sprint 34 file changes -no code update

--- env.qaInt.ts
+++ env.qaInt.ts
@@ -3,13 +3,13 @@ export const environment = {
     baseUrl: 'https://qa-content.montefioreeinstein.org/',
     cookies: {
         name: 'SSESS4259417db96d324c1be8c15c64a74f10',
-        value: '92okhqf8TC4FeEMr-ebEqcldRwyhDgQ2j8VnFWoY6BpQMKV5',
+        value: 'KcjktvgJmkTGon91NmqZMIxPOU5K6SspTwApoYTnRhBrjZPf',
         domain: '.qa-content.montefioreeinstein.org',
         path: '/',
     },
     admin: {
         name: 'SSESS4259417db96d324c1be8c15c64a74f10',
-        value: 't96tuLog2YtDeXVmQQC1eidwOxZQf6rwFeacLeYfLZrJynTL',
+        value: '9C%2CLBELv%2Capm5au2kXfr1NUySTabzwRVTD1WkZ0iZnuiO56b',
         domain: '.qa-content.montefioreeinstein.org',
         path: '/',
     }
