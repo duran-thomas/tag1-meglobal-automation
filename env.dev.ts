3c95ff3c3c50e714502c4fa28cda66af75084332
dscotland <dscotland@qualityworkscg.com>
2024-03-14 16:28:01 -0500

    fix: ci 1. updated cookies for dev environment

--- env.dev.ts
+++ env.dev.ts
@@ -3,13 +3,13 @@ export const environment = {
     baseUrl: 'https://dev.montefioreeinstein.org/',
     cookies: {
         name: 'SSESS3c48e47203cb9aa7ab600153bda26aac',
-        value: 'pAxUijB2ZI3vipDCnQIgsxlWzSr4Ne0fjw-VVtcwgi8Gbu2j',
+        value: '7MHe4tk4EhQ5xhIlDvdi9bYUWZ51bsaTTstv647ycSJtord4',
         domain: '.dev.montefioreeinstein.org',
         path: '/',
     },
     admin: {
         name: 'SSESS3c48e47203cb9aa7ab600153bda26aac',
-        value: '5bU313osMCc%2C03%2CChUpPZGitpy%2CPhXKsIuK7bwv5G2ugHkoH',
+        value: '7MHe4tk4EhQ5xhIlDvdi9bYUWZ51bsaTTstv647ycSJtord4',
         domain: '.dev.montefioreeinstein.org',
         path: '/',
     }
