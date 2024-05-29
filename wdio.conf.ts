22a9c4cb6d39dcf2b338b1182191d104955292d1
OThomps <othompson@qualityworkscg.com>
2024-05-29 13:18:13 -0500

    Working on footer

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -98,7 +98,9 @@ export const config: Options.Testrunner = {
         indexList: ['/test/specs/components/indexList.e2e.ts'],
         sidebar: ['/test/specs/components/sidebar.e2e.ts'],
         vrt: ['/test/specs/visual/visualTest.e2e.ts'],
-        breadcrumbs: ['/test/specs/components/breadcrumbs.e2e.ts']
+        breadcrumbs: ['/test/specs/components/breadcrumbs.e2e.ts'],
+        footer: ['/test/specs/components/footer.e2e.ts'],
+
     },
     // Patterns to exclude.
     exclude: [
@@ -201,7 +203,7 @@ export const config: Options.Testrunner = {
         // [QualityWatcherService, {
         //     email: process.env.QUALITYWATCHER_EMAIL,// Your QualityWatcher email
         //     apiKey: process.env.QUALITYWATCHER_API_KEY, // Your QualityWatcher API key
-        //     testRunName: "[Release 0.41.4] Automated Regression",
+        //     testRunName: "[Release 0.42.3] Automated Regression",
         //     description: 'This test run was created by the automation suite.',
         //     projectId: 1,
         //     includeAllCases: false,
