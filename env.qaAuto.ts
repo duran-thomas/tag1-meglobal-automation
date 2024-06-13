518851db77ccbb983e81500b07cf78637836da04
Duran Thomas <22084776+duran-thomas@users.noreply.github.com>
2024-06-12 19:51:03 -0500

    Navigation analytics

--- env.qaAuto.ts
+++ env.qaAuto.ts
@@ -1,16 +1,16 @@
 export const environment = {
-    bypassURL: 'https://meda2022:meda2022@meglobalode7.prod.acquia-sites.com/',
-    baseUrl: 'https://meglobalode7.prod.acquia-sites.com/',
-    cookies: {
-        name: 'SSESSdf0d9aa5f85649894e921d4b01e00b05',
-        value: 'caR1c7knCark5joPRXpvTcRi8A2%2C0Mn5Z1-Q9Gpo4fpYpnSe',
-        domain: '.meglobalode7.prod.acquia-sites.com',
-        path: '/',
-    },
-    admin: {
-        name: 'SSESSdf0d9aa5f85649894e921d4b01e00b05',
-        value: 'p7Bb4AWIOjP2t9Pz4cxeWiqA6zUMtru4gG2EMjJR6pPoLjgv',
-        domain: '.meglobalode7.prod.acquia-sites.com',
-        path: '/',
-    }
+  bypassURL: "https://meda2022:meda2022@meglobalode7.prod.acquia-sites.com/",
+  baseUrl: "https://meglobalode7.prod.acquia-sites.com/",
+  cookies: {
+    name: "SSESSdf0d9aa5f85649894e921d4b01e00b05",
+    value: "F9-mxVYS%2Ch8EOCuoCnFKT7-rN5%2C-99nRJ6i-r3xLtCKBo4fk",
+    domain: ".meglobalode7.prod.acquia-sites.com",
+    path: "/",
+  },
+  admin: {
+    name: "SSESSdf0d9aa5f85649894e921d4b01e00b05",
+    value: "2FJvTTyw54z-lADgqRRHOJdE08U4j29xygjV4oIMu5MvOkF3",
+    domain: ".meglobalode7.prod.acquia-sites.com",
+    path: "/",
+  },
 };
