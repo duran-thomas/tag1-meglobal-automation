caff64c34247fefb5e63e5844e90464eee668629
OThomps <othompson@qualityworkscg.com>
2024-07-03 15:06:23 -0500

    Updated poc to run test independently

---
+++ test/specs/visual/puppeteerScreenshot.js
@@ -0,0 +1,13 @@
+// const puppeteer = require('puppeteer');
+// const fs = require('fs');
+// const path = require('path');
+
+// async function captureFullPageScreenshot(url, screenshotPath) {
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.goto(url);
+//     await page.screenshot({ path: screenshotPath, fullPage: true });
+//     await browser.close();
+// }
+
+// module.exports = captureFullPageScreenshot;
